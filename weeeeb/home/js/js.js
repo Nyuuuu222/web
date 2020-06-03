410f84e81a06035b58ee549c5d47371ea48df032
Nyuuuu222 <64953570+Nyuuuu222@users.noreply.github.com>
2020-06-03 12:29:58 +0900

    d
    d

--- weeeeb/home/js/js.js
+++ weeeeb/home/js/js.js
@@ -1,3 +1,40 @@
+
+// 메뉴스크롤이동 쌤
+$(function(){
+    $('.btn_1').click(function(){
+       location.href = '.section04';
+    });
+});
+	/**스크롤이 설정한 높이 이상 내려갔을때 스타일추가하기**/
+	if (jQuery(window).width() > 0) {
+        jQuery(window).on("scroll",function(ev){
+            if(jQuery(window).scrollTop() > 150 ) { /**높이 픽셀 조정**/
+                jQuery('.menu_box').addClass('fixed');  /**위의 높이에서 .fixed 클래스를 추가합니다. 스타일에서 자유롭게 수치 조절 가능합니다.**/
+            }
+            else{
+                jQuery('.menu_box').removeClass('fixed');
+            }
+            return false;
+        });
+        }
+        
+        /**마우스 부드럽게 해당위치로 이동**/
+        $(function(){
+           $('a[href^=#]').click(function() {
+              var speed = 800;
+              var href= $(this).attr("href");
+              var target = $(href == "#" || href == "" ? 'html' : href);
+              var position = target.offset().top;
+              $('body,html').animate({scrollTop:position}, speed, 'swing');
+              return false;
+           });
+        });
+
+
+
+
+
+
 // 네비게이션
 // $(".btn_1").click(function () {
 //     var position = $("#window1").offset();
