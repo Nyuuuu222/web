2015fb15e348ed1e700969ffb0f6ed0bcc4572ee
baesukyung <64953394+baesukyung@users.noreply.github.com>
2020-06-29 15:29:38 +0900

    dd

--- car/car/js/js.js
+++ car/car/js/js.js
@@ -1,3 +1,5 @@
+// car color
+
 $(function(){
     $('#wc9').click(function(){
         $('.wc9').fadeIn();
@@ -75,69 +77,4 @@ $(function(){
 
 
 
-
-// $(function(){
-//     $('#wc9').click(function(){
-//         $('.wc9').stop().fadeIn();
-//         $('.r2t').stop().fadeOut();
-//         $('.e2r').stop().fadeOut();
-//         $('.q2w').stop().fadeOut();
-//         $('.j2f').stop().fadeOut();
-//         $('.a2s').stop().fadeOut();
-//         $('.y2b').stop().fadeOut();
-//     })
-//     $('#r2t').click(function(){
-//         $('.r2t').stop().fadeIn();
-//         $('.wc9').stop().fadeOut();
-//         $('.e2r').stop().fadeOut();
-//         $('.q2w').stop().fadeOut();
-//         $('.j2f').stop().fadeOut();
-//         $('.a2s').stop().fadeOut();
-//         $('.y2b').stop().fadeOut();
-//     })
-//     $('#e2r').click(function(){
-//         $('.e2r').stop().fadeIn();
-//         $('.wc9').stop().fadeOut();
-//         $('.r2t').stop().fadeOut();
-//         $('.q2w').stop().fadeOut();
-//         $('.j2f').stop().fadeOut();
-//         $('.a2s').stop().fadeOut();
-//         $('.y2b').stop().fadeOut();
-//     })
-//     $('#q2w').click(function(){
-//         $('.q2w').stop().fadeIn();
-//         $('.wc9').stop().fadeOut();
-//         $('.e2r').stop().fadeOut();
-//         $('.r2t').stop().fadeOut();
-//         $('.j2f').stop().fadeOut();
-//         $('.a2s').stop().fadeOut();
-//         $('.y2b').stop().fadeOut();
-//     })
-//     $('#j2f').click(function(){
-//         $('.j2f').stop().fadeIn();
-//         $('.wc9').stop().fadeOut();
-//         $('.e2r').stop().fadeOut();
-//         $('.q2w').stop().fadeOut();
-//         $('.r2t').stop().fadeOut();
-//         $('.a2s').stop().fadeOut();
-//         $('.y2b').stop().fadeOut();
-//     })
-//     $('#a2s').click(function(){
-//         $('.a2s').stop().fadeIn();
-//         $('.wc9').stop().fadeOut();
-//         $('.e2r').stop().fadeOut();
-//         $('.q2w').stop().fadeOut();
-//         $('.j2f').stop().fadeOut();
-//         $('.r2t').stop().fadeOut();
-//         $('.y2b').stop().fadeOut();
-//     })
-//     $('#y2b').click(function(){
-//         $('.y2b').stop().fadeIn();
-//         $('.wc9').stop().fadeOut();
-//         $('.e2r').stop().fadeOut();
-//         $('.q2w').stop().fadeOut();
-//         $('.j2f').stop().fadeOut();
-//         $('.a2s').stop().fadeOut();
-//         $('.r2t').stop().fadeOut();
-//     })
-// })
+// modal
